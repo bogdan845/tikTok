86207c927f6c27692b7a03cd6533538b80d1c07f
845 <lolalllolzgame@gmail.com>
2021-06-19 21:18:14 +0300

    first

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, FlatList, ScrollView } from "react-native";
+
+import { VideoContainer } from "./src/VideoContainer";
+import { DATA } from "./src/data";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        style={styles.videoList}
+        data={DATA}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => <VideoContainer video={item} />}
+      />
+
+      {/* <ScrollView contentContainerStyle={{flexGrow: 1, flex: 1}}>
+        {DATA.map((item) => (
+          <VideoContainer video={item} key={item.id}></VideoContainer>
+        ))}
+      </ScrollView> */}
     </View>
   );
 }
@@ -14,8 +26,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: 'relative'
+    // backgroundColor: "#4578ee",
+  },
+  videoList: {
+    position: 'relative',
+    // height: '100%',
+    // width: '100%',
+    flex: 1
   },
 });
---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const DATA = [ 
+    {
+        id: 1,
+        title: 'notBad',
+        uri: 'https://www.youtube.com/watch?v=5nl7sKwPQo4&ab_channel=NuclearBlastRecords'
+    },
+    {
+        id: 2,
+        title: 'tryToImprove',
+        uri: 'https://www.youtube.com/watch?v=5nl7sKwPQo4&ab_channel=NuclearBlastRecords'
+    },
+    {
+        id: 3,
+        title: 'thug_lifer',
+        uri: 'https://www.youtube.com/watch?v=067YF8KHdTM&ab_channel=Pain'
+    },
+    {
+        id: 4,
+        title: 'optNano',
+        uri: 'https://www.youtube.com/watch?v=067YF8KHdTM&ab_channel=Pain'
+    }
+    // ,
+    // {
+    //     id: 5,
+    //     title:
+    //     uri:
+    // },
+    // {
+    //     id: 6,
+    //     title:
+    //     uri:
+    // },
+    // {
+    //     id: 7
+    //     title:
+    //     uri:
+    // },
+    // {
+    //     id: 8,
+    //     title:
+    //     uri:
+    // },
+    // {
+    //     id: 9,
+    //     title:
+    //     uri:
+    // },
+    // {
+    //     id: 10,
+    //     title:
+    //     uri:
+    // },
+    // {
+    //     id: 11,
+    //     title:
+    //     uri:
+    // },
+    // {
+    //     id: 12,
+    //     title:
+    //     uri:
+    // },
+    // {
+    //     id: 13,
+    //     title:
+    //     uri:
+    // },
+    // {
+    //     id: 14,
+    //     title:
+    //     uri:
+    // },
+    // {
+    //     id: 15,
+    //     title:
+    //     uri:
+    // }
+    
+]
